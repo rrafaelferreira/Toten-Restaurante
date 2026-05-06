ce0e56bc52fe1b37fdee4d065bb15a0cae761157
rrafaelferreira <155680116+rrafaelferreira@users.noreply.github.com>
2026-05-05 21:31:19 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
-import { useState } from 'react'
-import ProductSection from './components/ProductSection/ProductsSection'
-import CarouselStructure from "./components/carousel/CarouselStructure";
-
 function App() {
-
-    const [selectedCategory, setSelectedCategory] =
-        useState('hamburgers')
+    const [selectedCategory, setSelectedCategory] = useState('hamburgers');
 
     return (
         <div className='main-app-container'>
-            <CarouselStructure
-                selectedCategory={selectedCategory}
-                setSelectedCategory={setSelectedCategory}
-            />
+            {/* Criamos uma div que abraça os dois e tem o fundo preto */}
+            <div className="bg-secao-produtos" style={{ backgroundColor: '#111' }}>
+                <CarouselStructure
+                    selectedCategory={selectedCategory}
+                    setSelectedCategory={setSelectedCategory}
+                />
 
-            <ProductSection
-                selectedCategory={selectedCategory}
-            />
+                <ProductSection
+                    selectedCategory={selectedCategory}
+                />
+            </div>
         </div>
-    )
+    );
 }
-
-export default App
