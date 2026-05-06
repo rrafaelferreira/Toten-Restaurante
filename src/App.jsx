434dc0b3b0e523c5091f35f6bb510fc7e35c88e7
rrafaelferreira <155680116+rrafaelferreira@users.noreply.github.com>
2026-05-06 00:34:00 -0300

    Corrindo alguns visuais

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { useState } from 'react'
+import ProductSection from './components/ProductSection/ProductsSection'
+import CarouselStructure from "./components/carousel/CarouselStructure";
+
 function App() {
     const [selectedCategory, setSelectedCategory] = useState('hamburgers');
 
     return (
         <div className='main-app-container'>
             {/* Criamos uma div que abraça os dois e tem o fundo preto */}
-            <div className="bg-secao-produtos" style={{ backgroundColor: '#111' }}>
+            <div className="bg-secao-produtos" style={{ backgroundColor: '#111', minHeight: '100vh' }}>
                 <CarouselStructure
                     selectedCategory={selectedCategory}
                     setSelectedCategory={setSelectedCategory}
@@ -17,3 +21,5 @@ function App() {
         </div>
     );
 }
+
+export default App
