9c8d0a3580841fbaf151183b6519d5749408fa79
rrafaelferreira <155680116+rrafaelferreira@users.noreply.github.com>
2026-05-12 00:08:16 -0300

    fix: corrige conflito de routers e ajusta base path para deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { CartProvider } from "./Shop/Context/CartContext";
 
-// Importação das páginas com os nomes exatos dos ficheiros
+// Importação das páginas (verifique se os nomes batem com as pastas reais!)
 import Home from "./pages/home/Home";
 import OrderType from "./pages/OrderType/OrderType";
-import Menu from "./pages/Menu/Menu";
+import Menu from "./pages/Menu/Menu"; 
 import ShoppingCart from "./pages/Cart/ShoppingCart";
 
 import "./App.css";
@@ -12,14 +12,13 @@ import "./App.css";
 function App() {
   return (
     <CartProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/order-type" element={<OrderType />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/cart" element={<ShoppingCart />} />
-        </Routes>
-      </Router>
+      {/* O <Router> foi removido daqui porque já está no main.jsx */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/order-type" element={<OrderType />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/cart" element={<ShoppingCart />} />
+      </Routes>
     </CartProvider>
   );
 }
